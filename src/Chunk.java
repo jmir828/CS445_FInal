9367ca5dac67c92a44f2f806e1a487688e8faaec
joswizzle <joswizzle@joswizzles-Comp>
2015-11-13 12:21:21 -0800

    Updated Classes

--- src/Chunk.java
+++ src/Chunk.java
@@ -5,6 +5,9 @@ import java.util.Random;
 import org.lwjgl.BufferUtils;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class Chunk {
     static final int CHUNK_SIZE = 30;
@@ -14,6 +17,8 @@ public class Chunk {
     private int VBOColorHandle;
     private int StartX, StartY, StartZ;
     private Random r;
+    private int VBOTextureHandle;
+    private Texture texture;
     
     public void render(){
         glPushMatrix();
@@ -24,6 +29,11 @@ public class Chunk {
         glBindBuffer(GL_ARRAY_BUFFER,
         VBOColorHandle);
         glColorPointer(3,GL_FLOAT, 0, 0L);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
+        glBindTexture(GL_TEXTURE_2D, 1);
+        glTexCoordPointer(2,GL_FLOAT,0,0L);
+        
         glDrawArrays(GL_QUADS, 0,
         CHUNK_SIZE *CHUNK_SIZE*
         CHUNK_SIZE * 24);
@@ -31,8 +41,13 @@ public class Chunk {
     }
     
     public void rebuildMesh(float startX, float startY, float startZ) {
+        SimplexNoise noise = new SimplexNoise(4,1.0,5);
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers();
+        
+        FloatBuffer VertexTextureData = 
+                BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE *CHUNK_SIZE) * 6 * 12);
         FloatBuffer VertexPositionData =
                 BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE *CHUNK_SIZE) * 6 * 12);
         FloatBuffer VertexColorData =
@@ -40,21 +55,26 @@ public class Chunk {
         for (float x = 0; x < CHUNK_SIZE; x += 1) {
             for (float z = 0; z < CHUNK_SIZE; z += 1) {
                 for(float y = 0; y < CHUNK_SIZE; y++){
-                    VertexPositionData.put( createCube((float) (startX + x* CUBE_LENGTH),
+                    VertexPositionData.put(createCube((float) (startX + x* CUBE_LENGTH),
                                             (float)(y*CUBE_LENGTH+(int)(CHUNK_SIZE*.8)),
                                             (float) (startZ + z *CUBE_LENGTH)));
                     VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
+                    VertexTextureData.put(createTexCube((float) 0, (float) 0,Blocks[(int)(x)][(int) (y)][(int) (z)]));
                 }
             }
         }
         
         VertexColorData.flip();
         VertexPositionData.flip();
+        VertexTextureData.flip();
         glBindBuffer(GL_ARRAY_BUFFER,VBOVertexHandle);
         glBufferData(GL_ARRAY_BUFFER,VertexPositionData,GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ARRAY_BUFFER,VBOColorHandle);
         glBufferData(GL_ARRAY_BUFFER,VertexColorData,GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexTextureData, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     }
@@ -102,18 +122,72 @@ public class Chunk {
     }
     
     private float[] getCubeColor(Block block) {
-        switch (block.GetID()) {
-            case 1:
-                return new float[] { 0, 1, 0 };
-            case 2:
-                return new float[] { 1, 0.5f, 0 };
-            case 3:
-                return new float[] { 0, 0f, 1f };
-        }
+//        switch (block.GetID()) {
+//            case 1:
+//                return new float[] { 0, 1, 0 };
+//            case 2:
+//                return new float[] { 1, 0.5f, 0 };
+//            case 3:
+//                return new float[] { 0, 0f, 1f };
+//        }
         return new float[] { 1, 1, 1 };
     }
     
+    public static float[] createTexCube(float x, float y, Block block) {
+        float offset = (1024f/16)/1024f;
+        
+        switch (block.GetID()) {
+            case 1:
+                return new float[] {
+                // BOTTOM QUAD(DOWN=+Y)
+                x + offset*3, y + offset*10,
+                x + offset*2, y + offset*10,
+                x + offset*2, y + offset*9,
+                x + offset*3, y + offset*9,
+                // TOP!
+                x + offset*3, y + offset*1,
+                x + offset*2, y + offset*1,
+                x + offset*2, y + offset*0,
+                x + offset*3, y + offset*0,
+                // FRONT QUAD
+                x + offset*3, y + offset*0,
+                x + offset*4, y + offset*0,
+                x + offset*4, y + offset*1,
+                x + offset*3, y + offset*1,
+                // BACK QUAD
+                x + offset*4, y + offset*1,
+                x + offset*3, y + offset*1,
+                x + offset*3, y + offset*0,
+                x + offset*4, y + offset*0,
+                // LEFT QUAD
+                x + offset*3, y + offset*0,
+                x + offset*4, y + offset*0,
+                x + offset*4, y + offset*1,
+                x + offset*3, y + offset*1,
+                // RIGHT QUAD
+                x + offset*3, y + offset*0,
+                x + offset*4, y + offset*0,
+                x + offset*4, y + offset*1,
+                x + offset*3, y + offset*1}; 
+            case 2:
+                
+            case 3:
+                
+            case 4:
+                
+            case 5:
+                
+            default: return new float[] { 1, 1, 1 };
+        }
+        
+    }
+    
     public Chunk(int startX, int startY, int startZ) {
+        try{texture = TextureLoader.getTexture("PNG", 
+                ResourceLoader.getResourceAsStream("terrain.png"));
+        }
+        catch(Exception e) { System.out.print("ER-ROAR!"); }
+        
         r= new Random();
         Blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
         
@@ -132,8 +206,10 @@ public class Chunk {
                 }
             }
         }
+        
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers();
         StartX = startX;
         StartY = startY;
         StartZ = startZ;
